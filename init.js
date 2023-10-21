0231cbcda795a003f31f5291c72a1c8f743b8dc3
JoshCLWren <joshisplutar@gmail.com>
2023-10-21 12:26:40 -0500

    Update init.js

--- init.js
+++ init.js
@@ -1,130 +1,117 @@
-var button = document.getElementById('button');
-var playerContainer = [];
+const button = document.getElementById('button');
+const playerContainer = [];
 
 class Character {
-  constructor(playerName, name, init, race){
-  this.playerName = playerName;  
-  this.name = name;
-  this.init = Number(init);
-  this.race = race;
-}
-  getPlayerName(){
+  constructor(playerName, name, init, race) {
+    this.playerName = playerName;
+    this.name = name;
+    this.init = Number(init);
+    this.race = race;
+  }
+
+  getPlayerName() {
     return this.playerName;
   }
-  getName(){
+
+  getName() {
     return this.name;
   }
-  getInit(){
+
+  getInit() {
     return this.init;
   }
-  getRace(){
+
+  getRace() {
     return this.race;
   }
 }
 
-function printCharacter(character){
-  var node = document.createElement("P");
-  var img = document.createElement("IMG");
-  var div = document.createElement('DIV');
-  var imgdiv = document.createElement('div');
-  var textNode = document.createTextNode(character.playerName + character.name + character.init + character.race);
+function printCharacter(character) {
+  const node = document.createElement("P");
+  const img = document.createElement("IMG");
+  const div = document.createElement('DIV');
+  const imgdiv = document.createElement('div');
+  const textNode = document.createTextNode(`${character.playerName} ${character.name} ${character.init} ${character.race}`);
   node.appendChild(textNode);
   div.appendChild(node);
-  event.preventDefault()
   div.setAttribute('class', 'portrait');
 
-  if(character.race === "human"){
-    img.setAttribute('src', 'humanfighter.jpg');
-  }
-  else if(character.race === "elf"){
-    img.setAttribute('src', 'elfwizard.jpg');
-
-  } else if(character.race === "dwarf"){
-    img.setAttribute('src', 'dwarfcleric.jpg');
+  const raceImages = {
+    "human": 'humanfighter.jpg',
+    "elf": 'elfwizard.jpg',
+    "dwarf": 'dwarfcleric.jpg'
   };
+
+  img.setAttribute('src', raceImages[character.race] || '');
   imgdiv.appendChild(img);
   div.appendChild(imgdiv);
   document.getElementById("gallery").appendChild(div);
 }
 
-function imager() {
-
+function loopOverCharacter(playerContainer) {
+  playerContainer.forEach(printCharacter);
 }
 
-function loopOverCharacter(playerContainer){
-  for(var i = 0; i < playerContainer.length; i++){
-    printCharacter(playerContainer[i]);
-  }
-}
-
-
-function destroyAllCharacters(){
+function destroyAllCharacters() {
   document.getElementById('gallery').innerHTML = '';
 }
 
-function pcCreator(){
+function pcCreator(event) {
   event.preventDefault();
   
-  var name = document.getElementById('name').value;
-  var init = document.getElementById('init').value;
-  var race = document.getElementById('race').value;
-  var playerName = document.getElementById("Player-Name").value;  
+  const name = document.getElementById('name').value;
+  const init = document.getElementById('init').value;
+  const race = document.getElementById('race').value;
+  const playerName = document.getElementById("Player-Name").value;
+  
   playerContainer.push(new Character(playerName, name, init, race));
-  playerContainer.sort(function(a, b){
-    if(a.init > b.init){
-      return -1;
-    } 
-    if (a.init < b.init){
-      return 1;
-    }
-    return 0;
-  })
+  playerContainer.sort((a, b) => b.init - a.init);
   destroyAllCharacters();
-  console.log(playerContainer);
   loopOverCharacter(playerContainer);
 }
 
-
-function hideEnemyCreator(){
-  document.getElementById('enemyCreator').style.display ='none';
+function hideEnemyCreator() {
+  document.getElementById('enemyCreator').style.display = 'none';
 }
-function showEnemyCreator(){
+
+function showEnemyCreator() {
   document.getElementById('enemyCreator').style.display = 'block';
 }    
 
-function hidePcCreator(){
-  document.getElementById('pcCreator').style.display ='none';
+function hidePcCreator() {
+  document.getElementById('pcCreator').style.display = 'none';
 }
-function showPcCreator(){
+
+function showPcCreator() {
   document.getElementById('pcCreator').style.display = 'block';
 }    
 
-function hideLairCreator(){
-  document.getElementById('lairCreator').style.display ='none';
+function hideLairCreator() {
+  document.getElementById('lairCreator').style.display = 'none';
 }
-function showLairCreator(){
+
+function showLairCreator() {
   document.getElementById('lairCreator').style.display = 'block';
 }      
 
-function enemyCreator(){
-  var node = document.createElement("P");
-  var name = document.getElementById('enemyName').value;
-  var init = document.getElementById('enemyInit').value;
-  var img = document.createElement("IMG");
-  var textNode = document.createTextNode(name +" has an iniative of:" + init);
+function enemyCreator(event) {
+  event.preventDefault();
+
+  const node = document.createElement("P");
+  const name = document.getElementById('enemyName').value;
+  const init = document.getElementById('enemyInit').value;
+  const textNode = document.createTextNode(`${name} has an initiative of: ${init}`);
   node.appendChild(textNode);
   document.getElementById("gallery").appendChild(node);
-  event.preventDefault()}
+}
 
-function lairCreator(){
-  var node = document.createElement("P");
-  var name = document.getElementById('lairName').value;
-  var init = document.getElementById('lairInit').value;
-  var img = document.createElement("IMG");
-  var textNode = document.createTextNode(name +" has an iniative of:" + init);
+function lairCreator(event) {
+  event.preventDefault();
+
+  const node = document.createElement("P");
+  const name = document.getElementById('lairName').value;
+  const init = document.getElementById('lairInit').value;
+  const textNode = document.createTextNode(`${name} has an initiative of: ${init}`);
   node.appendChild(textNode);
   document.getElementById("gallery").appendChild(node);
-  event.preventDefault()}
-
-
- 
+}
